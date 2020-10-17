0ebb8979e852fc58b0816ae70433ae152c010ac0
yamatoto <mftb35ymt@yahoo.co.jp>
2020-10-17 10:24:04 +0900

    eslint設定、setting.js記述

--- pages/index.vue
+++ pages/index.vue
@@ -1,19 +1,11 @@
 <template>
   <div class="container">
     <div>
-      <logo />
-      <h1 class="title">
-        todo-app
-      </h1>
-      <h2 class="subtitle">
-        My first-rate Nuxt.js project
-      </h2>
+      <Logo />
+      <h1 class="title">todo-app</h1>
+      <h2 class="subtitle">My first-rate Nuxt.js project</h2>
       <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green"
-        >
+        <a href="https://nuxtjs.org/" target="_blank" class="button--green">
           Documentation
         </a>
         <a
@@ -29,14 +21,14 @@
 </template>
 
 <script lang="ts">
-import Vue from 'vue'
-import Logo from '~/components/Logo.vue'
+import Vue from 'vue';
+import Logo from '~/components/Logo.vue';
 
 export default Vue.extend({
   components: {
     Logo
   }
-})
+});
 </script>
 
 <style>
